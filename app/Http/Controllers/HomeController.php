1fbc64729add94451904ead3e23930ff699dc614
NikolozTchitanava <143025948+NikolozTchitanava@users.noreply.github.com>
2025-03-13 17:39:13 +0700

    added home page(very basic version)including HomeController,home.blade.php and updated web.php

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Welcome to Memeviibe',
+            'message' => 'This is the initial  home page of Memeviibe!',
+        ];
+
+        return view('home', $data);
+    }
+}
